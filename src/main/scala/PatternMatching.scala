31b1cedae9697a5397b7beff72c80d62d7bfbd21
Felipe Macedo <felipe.x.macedo@gmail.com>
2025-04-28 21:47:45 -0300

    Pattern guards

--- src/main/scala/PatternMatching.scala
+++ src/main/scala/PatternMatching.scala
@@ -92,4 +92,37 @@ class PatternMatching {
     case s"$name is ${Age(age)} years old" => (name, age)
   // name: String = Alice
   // age: Int = 25
+
+  //Pattern guards
+  
+  //Pattern guards are boolean expressions which are used to make cases more specific. 
+  // Just add if <boolean expression> after the pattern.
+  def showImportantNotification(notification: Notification, importantPeopleInfo: Seq[String]): String =
+    notification match
+      case Email(sender, _, _) if importantPeopleInfo.contains(sender) =>
+        "You got an email from special someone!"
+      case SMS(number, _) if importantPeopleInfo.contains(number) =>
+        "You got an SMS from special someone!"
+      case other =>
+        showNotification(other) // nothing special, delegate to our original showNotification function
+
+  val importantPeopleInfo = Seq("867-5309", "jenny@gmail.com")
+
+  val someSms = SMS("123-4567", "Are you there?")
+  val someVoiceRecording = VoiceRecording("Tom", "voicerecording.org/id/123")
+  val importantEmail = Email("jenny@gmail.com", "Drinks tonight?", "I'm free after 5!")
+  val importantSms = SMS("867-5309", "I'm here! Where are you?")
+
+  println(showImportantNotification(someSms, importantPeopleInfo))
+  // prints You got an SMS from 123-4567! Message: Are you there?
+  
+  println(showImportantNotification(someVoiceRecording, importantPeopleInfo))
+  //(It's wrong) 
+  // prints You received a Voice Recording from Tom! Click the link to hear it: voicerecording.org/id/123 
+  
+  println(showImportantNotification(importantEmail, importantPeopleInfo)) 
+  // prints You got an email from special someone!
+  
+
+  println(showImportantNotification(importantSms, importantPeopleInfo)) // prints You got an SMS from special someone!
 }
