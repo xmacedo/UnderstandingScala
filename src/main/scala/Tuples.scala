a730f68c43c6d24ca002127f065a40f217859dda
Felipe Macedo <felipe.x.macedo@gmail.com>
2025-05-01 13:32:38 -0300

    Tuples

--- src/main/scala/Tuples.scala
+++ src/main/scala/Tuples.scala
@@ -1,4 +1,17 @@
 class Tuples {
   //https://docs.scala-lang.org/tour/tuples.html
 
+  //Tuples
+
+  //In Scala, a tuple is a value that contains a fixed number of elements, each with its own type.
+  // Tuples are immutable.
+
+  //Tuples are especially handy for returning multiple values from a method.
+  //A tuple with two elements can be created as follows:
+  val ingredient = ("Sugar", 25)
+
+  //This creates a tuple containing a String element and an Int element.
+  //The inferred type of ingredient is (String, Int).
+
+  //
 }
