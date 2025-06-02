f2b0ca320230707dd822e2ebfd928dd7a77a842f
Felipe Macedo <felipe.x.macedo@gmail.com>
2025-06-02 19:07:33 -0300

    Mutable and Immutable Collections

---
+++ src/main/scala/MutableAndImmutableCollections.scala
@@ -0,0 +1,11 @@
+class MutableAndImmutableCollections {
+  //https://docs.scala-lang.org/overviews/collections-2.13/overview.html
+  //Mutable and Immutable Collections
+  
+  //Scala collections systematically distinguish between mutable and immutable collections. 
+  // A mutable collection can be updated, reduced or extended in place. This means you can change, add, 
+  // or remove elements of a collection as a side effect. Immutable collections, by contrast, never change. 
+  // You still have operations that simulate additions, removals, or updates, but those operations will in 
+  // each case return a new collection and leave the old collection unchanged.
+  
+}
