66d4f471de9bc75ca5dd047fb629312cf963f577
Felipe Macedo <felipe.x.macedo@gmail.com>
2025-06-04 10:23:06 -0300

    Mutable and Immutable Collections:

--- src/main/scala/MutableAndImmutableCollections.scala
+++ src/main/scala/MutableAndImmutableCollections.scala
@@ -12,13 +12,21 @@ class MutableAndImmutableCollections {
   // Most collection classes needed by client code exist in three variants, which are located in packages 
   // scala.collection, scala.collection.immutable, and scala.collection.mutable, respectively.
   // Each variant has different characteristics with respect to mutability.
-  
-  //A collection in package scala.collection.immutable is guaranteed to be immutable for everyone. 
-  // Such a collection will never change after it is created. Therefore, you can rely on the fact that accessing the 
+
+  //A collection in package scala.collection.immutable is guaranteed to be immutable for everyone.
+  // Such a collection will never change after it is created. Therefore, you can rely on the fact that accessing the
   // same collection value repeatedly at different points in time will always yield a collection with the same elements.
-  
-  //A collection in package scala.collection.mutable is known to have some operations that change the collection in place. 
+
+  //A collection in package scala.collection.mutable is known to have some operations that change the collection in place.
   // So dealing with a mutable collection means you need to understand which code changes which collection when.
   
-  
+  //A collection in package scala.collection can be either mutable or immutable. 
+  // For instance, collection.IndexedSeq[T] is a superclass of both collection.immutable.IndexedSeq[T] 
+  // and collection.mutable.IndexedSeq[T]. 
+  // Generally, the root collections in package scala.collection support transformation operations affecting 
+  // the whole collection, the immutable collections in package scala.collection.immutable typically 
+  // add operations for adding or removing single values, and the mutable collections in package 
+  // scala.collection.mutable typically add some side-effecting modification operations to the root interface.
+
+
 }
