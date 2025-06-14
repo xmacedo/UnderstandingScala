ae96fcbeb7085e34d7f95ca8731ac7731c4dbd62
Felipe Macedo <felipe.x.macedo@gmail.com>
2025-06-14 19:26:39 -0300

    Functions that accept functions

--- src/main/scala/HigherOrderFunctions.scala
+++ src/main/scala/HigherOrderFunctions.scala
@@ -89,4 +89,24 @@ class HigherOrderFunctions {
   // Therefore, having functions that compose based on other functions can allow us to build more generic mechanisms. 
   // Such generic operations avoid completely locking down their behaviour in order to give clients a way 
   // to control or further customize parts of those operations.
+  
+  //Functions that return functions
+  //There are certain cases where you want to generate a function. Here’s an example of a method that returns 
+  // a function.
+  def urlBuilder(ssl: Boolean, domainName: String): (String, String) => String =
+    val schema = if ssl then "https://" else "http://"
+    (endpoint: String, query: String) => s"$schema$domainName/$endpoint?$query"
+
+  val domainName = "www.example.com"
+
+  def getURL = urlBuilder(ssl = true, domainName)
+
+  val endpoint = "users"
+  val query = "id=1"
+  val url = getURL(endpoint, query) // "https://www.example.com/users?id=1": String
+  
+  //Notice the return type of urlBuilder (String, String) => String. 
+  // This means that the returned anonymous function takes two Strings and returns a String. 
+  // In this case, the returned anonymous function is 
+  // (endpoint: String, query: String) => s"https://www.example.com/$endpoint?$query".
 }
