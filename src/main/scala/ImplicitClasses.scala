a43d8c750db561a9cc993193c92f8fde1e5a2480
Felipe Macedo <felipe.x.macedo@gmail.com>
2025-09-04 23:21:59 -0300

    Implicit classes

--- src/main/scala/ImplicitClasses.scala
+++ src/main/scala/ImplicitClasses.scala
@@ -1,4 +1,17 @@
 class ImplicitClasses {
   //https://docs.scala-lang.org/overviews/core/implicit-classes.html#inner-main
+  // This doc page is specific to features shipped in Scala 2, which have either been removed in Scala 3 or replaced 
+  // by an alternative. Unless otherwise stated, all the code examples in this page assume you are using Scala 2.
 
+  //In Scala 3, implicit classes are still supported for compatibility reasons but the recommended way to achieve 
+  // the same result is to use extension methods (https://docs.scala-lang.org/scala3/book/ca-extension-methods.html).
+  
+  //Introduction
+  //Scala 2.10 introduced a new feature called implicit classes. An implicit class is a class marked with the implicit 
+  // keyword. This keyword makes the class’s primary constructor available for implicit conversions when the 
+  // class is in scope.
+  
+  //Implicit classes were proposed in SIP-13. (https://docs.scala-lang.org/sips/implicit-classes.html)
+  
+  
 }
